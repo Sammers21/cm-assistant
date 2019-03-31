e3ecfe54c2023241e503bdae9cb9828eee464476
Sammers21 <titantins@gmail.com>
2019-03-31 22:52:29 +0300

    make project use coroutines with vertx http request to dotabuff/heroes

--- picker/src/main/kotlin/io/github/Sammers21/cm/picker/impl/DotabuffClientImpl.kt
+++ picker/src/main/kotlin/io/github/Sammers21/cm/picker/impl/DotabuffClientImpl.kt
@@ -2,13 +2,28 @@ package io.github.Sammers21.cm.picker.impl
 
 import io.github.Sammers21.cm.picker.CounterInfo
 import io.github.Sammers21.cm.picker.DotabuffClient
+import io.vertx.core.Vertx
+import io.vertx.ext.web.client.WebClient
+import io.vertx.kotlin.ext.web.client.sendAwait
 
 class DotabuffClientImpl : DotabuffClient {
-    override fun heroes(): List<String> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+    private val vertx: Vertx
+    private val webClient: WebClient
+
+    constructor(vertx: Vertx) {
+        this.vertx = vertx
+        this.webClient = WebClient.create(vertx)
     }
 
-    override fun counters(hero: String): Map<String, CounterInfo> {
+    override suspend fun heroes(): List<String> {
+        val response = webClient.getAbs("https://www.dotabuff.com/heroes").sendAwait()
+        val bodyAsString = response.bodyAsString()
+        println(bodyAsString)
+        return listOf<String>("oracle")
+    }
+
+    override suspend fun counters(hero: String): Map<String, CounterInfo> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
--- picker/src/main/kotlin/io/github/Sammers21/cm/picker/main.kt
+++ picker/src/main/kotlin/io/github/Sammers21/cm/picker/main.kt
@@ -1,5 +1,19 @@
 package io.github.Sammers21.cm.picker
 
+import io.github.Sammers21.cm.picker.impl.DotabuffClientImpl
+import io.vertx.core.Vertx
+import io.vertx.kotlin.coroutines.dispatcher
+import kotlinx.coroutines.*
+
 fun main() {
-    println("Hello, world!")
+    runBlocking<Unit> {
+        val vertx = Vertx.vertx()
+        val dotabuffClientImpl = DotabuffClientImpl(vertx)
+        launch(vertx.dispatcher()) {
+            for (hero in dotabuffClientImpl.heroes()) {
+                println(hero)
+            }
+        }
+        dotabuffClientImpl.heroes();
+    }
 }
