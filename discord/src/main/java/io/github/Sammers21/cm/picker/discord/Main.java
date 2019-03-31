d066d1a6c16743eba66ba7413f5b773c42bc802b
Sammers21 <titantins@gmail.com>
2019-04-01 01:21:42 +0300

    add javacord with ping example

--- discord/src/main/java/io/github/Sammers21/cm/picker/discord/Main.java
+++ discord/src/main/java/io/github/Sammers21/cm/picker/discord/Main.java
@@ -1,7 +1,21 @@
 package io.github.Sammers21.cm.picker.discord;
 
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+
 public class Main {
     public static void main(String[] args) {
+        String token = args[0];
+        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
 
+        // Add a listener which answers with "Pong!" if someone writes "!ping"
+        api.addMessageCreateListener(event -> {
+            if (event.getMessage().getContent().equalsIgnoreCase("!ping")) {
+                event.getChannel().sendMessage("Pong!");
+            }
+        });
+
+        // Print the invite url of your bot
+        System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
     }
 }
