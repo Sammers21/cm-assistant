b349bf2ba17039142ce4cb62bc41eb351e156f86
Pavel Drankov <titantins@gmail.com>
2019-05-03 00:24:52 +0300

    introduce aliases

--- picker/src/main/kotlin/io/github/Sammers21/cm/picker/Dota2Heroes.kt
+++ picker/src/main/kotlin/io/github/Sammers21/cm/picker/Dota2Heroes.kt
@@ -1,7 +1,30 @@
 package io.github.Sammers21.cm.picker
 
-data class Dota2Heroes(val heroes: Set<Hero>) {
+import java.util.concurrent.ConcurrentHashMap
 
+typealias HeroAlias = Pair<String, String>
+
+class Dota2Heroes(heroes: Set<Hero>, aliases: List<HeroAlias> = Aliases.DefaultAliases) {
+
+    var internalLookUpMap: Map<String, Hero> = ConcurrentHashMap()
+
+    init {
+        heroes.forEach { hero: Hero ->
+            internalLookUpMap.plus(hero.originalHeroName to hero)
+        }
+        aliases.forEach { alias: HeroAlias ->
+            val hero = internalLookUpMap.get(alias.second)
+            if (hero == null) {
+                throw IllegalStateException(
+                        String.format("%s is not a valid alias for %s, because %s is an invalid hero name", alias.first, alias.second, alias.first)
+                )
+            } else {
+                internalLookUpMap.plus(alias.first to hero)
+            }
+        }
+    }
+
+    fun lookUpHero(name: String): Hero? {
+        return internalLookUpMap.get(name);
+    }
 }
-
-object Do
